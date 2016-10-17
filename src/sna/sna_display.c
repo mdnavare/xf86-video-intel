mdnavare/xf86-video-intel

commit eb01cc549d4d5544833ff6da4d79bc63587fd5ca

sna: Refresh mode list if the kernel updates
If the kernel changes its list of modes, make sure we update. (This is a
losing battle since X will add whatever modes it finds from the EDID
despite what the kernel is telling it.)

Signed-off-by: Chris Wilson <chris@chris-wilson.co.uk>

src/sna/sna_display.c
@@ -5190,6 +5190,9 @@ output_check_status(struct sna *sna, struct sna_output *output)
 	if (status != XF86OutputStatusConnected)
 		return true;
 
+	if (output->num_modes != compat_conn.conn.count_modes)
+		return true;
+
 	if (output->edid_len == 0)
 		return false;
 

commit 2ef2590c1b234a8dee96d7a95a49e7d442100ee0

sna: Reprobe if kernel updates the connector mode list
Output_check_status() should return a false if it detects
that the connector mode list has changed so that sna_mode_discover
can reprobe.

Fixes: eb01cc549d4d (sna: Refresh mode list if the kernel updates)

Signed-off-by: Manasi Navare <manasi.d.navare@intel.com>
Cc: Chris Wilson <chris@chris-wilson.co.uk>
Cc: Ville Syrjala <ville.syrjala@linux.intel.com>

src/sna/sna_display.c
@@ -5191,7 +5191,7 @@ output_check_status(struct sna *sna, struct sna_output *output)
 		return true;
 
 	if (output->num_modes != compat_conn.conn.count_modes)
-		return true;
+		return false;
 
 	if (output->edid_len == 0)
 		return false;